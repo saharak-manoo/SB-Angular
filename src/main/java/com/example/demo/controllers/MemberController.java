fececaa2fadf8fb7e42cfefa05eed4204b797cc7
Saharak <Saharak@bananacoding.com>
2019-11-21 08:51:01 +0700

    [Setup] java mvc

---
+++ src/main/java/com/example/demo/controllers/MemberController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Member;
+import com.example.demo.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MemberController {
+  @Autowired
+  private MemberService memberService;
+
+  @GetMapping("/members")
+  public List<Member> all() {
+    return memberService.getAllMembers();
+  }
+
+  @PostMapping("/members")
+  public ResponseEntity<Member> createMember(@Valid @RequestBody Member member) {
+    return ResponseEntity.ok(memberService.saveMember(member));
+  }
+
+  @PutMapping("/members/{id}")
+  public ResponseEntity<Member> updateMember(@Valid @RequestBody Member member, @PathVariable(value = "id") Long id) {
+    return ResponseEntity.ok(memberService.updateMember(member, id));
+  }
+
+  @DeleteMapping("/members/{id}")
+  public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
+    Map<String, String> response = new HashMap<String, String>();
+    if (memberService.deleteMember(id)) {
+      response.put("status", "success");
+      response.put("message", "member deleted successfully");
+      return ResponseEntity.ok(response);
+    } else {
+      response.put("status", "error");
+      response.put("message", "Somthing went wrong when delete the member");
+      return ResponseEntity.status(500).body(response);
+    }
+  }
+}
---
+++ src/main/java/com/example/demo/models/Member.java
@@ -0,0 +1,31 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "members")
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "member_id")
+  private Long id;
+
+  @Column(name = "first_name")
+  @NotEmpty(message = "* Please Enter First Name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  @NotEmpty(message = "* Please Enter Last Name")
+  private String lastName;
+
+  @Email(message = "* Please Enter Valid Email Address")
+  @NotEmpty(message = " * Please Provide Email Address")
+  @Column(name = "email")
+  private String email;
+
+}
