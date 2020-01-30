29fb2ef2d86622b9adc3eb1946712e40c27e1227
Saharak <Saharakmanoo@outlook.com>
2020-01-30 13:24:10 +0700

    [Added] jwt

--- src/main/java/com/example/demo/services/JwtUserDetailsService.java
+++ src/main/java/com/example/demo/services/JwtUserDetailsService.java
@@ -2,7 +2,6 @@ package com.example.demo.services;
 
 import java.util.ArrayList;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,21 +9,20 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.models.User;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
-	private com.example.demo.models.User ModalUser;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		System.out.println(username);
-		ModalUser userData = userRepository.findByUsername(username);
-		System.out.println(userData);
-		if ("saharakmanoo".equals(username)) {
-			return new User("saharakmanoo", "$2a$10$ixlPY3AAd4ty1l6E2IsQ9OFZi2ba9ZQE0bP7RFcGIWNhyFrrT3YUi",
-					new ArrayList<>());
+		User user = userRepository.findByUsername(username);
+		if (user != null) {
+			return new org.springframework.security.core.userdetails.User(username, "$2a$10$ixlPY3AAd4ty1l6E2IsQ9OFZi2ba9ZQE0bP7RFcGIWNhyFrrT3YUi",
+							new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
